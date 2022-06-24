925efca5eb74cce9bf183245c420bd7eed977289
Chris Eun <eunchristopher@gmail.com>
2022-06-23 17:22:32 -0700

    graffiti project

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header');
+
+header.style.textAlign = "left";
+
+let dogImages = document.querySelectorAll('.dog-image');
+for (let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '10px';
+    dogImages[i].style.transform = 'rotate(180deg)';
+}
+
+
+let dogName = document.querySelectorAll('.dog-name');
+for (let i = 0; i < dogName.length; i++){
+  dogName[i].style.textAlign = "left";
+}
+
+let footer = document.querySelector('.footer');
+footer.style.color = 'red';
+footer.style.borderStyle = 'solid';
